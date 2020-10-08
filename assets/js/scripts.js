320605b2dfa3ed497e690b855aee9872c61445f6
Pedro <you@example.com>
2020-10-08 00:48:31 -0300

    Criando modal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,12 @@
-import Menu from './module/menu.js';
+import Menu from "./module/menu.js";
+import Modal from "./module/modal.js";
 
 const menu = new Menu('[data-menu="button"]', '[data-menu="list"]');
 menu.init();
+
+const modal = new Modal(
+  '[data-modal="open"]',
+  '[data-modal="close"]',
+  '[data-modal="container"]'
+);
+modal.init();
